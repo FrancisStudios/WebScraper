62fe9f7c34ead4b8e69114420c805ae798b672d7
unknown <vlfmap@gmail.com>
2023-07-17 09:47:05 +0200

    login method

--- cypress/e2e/START.cy.js
+++ cypress/e2e/START.cy.js
@@ -6,16 +6,35 @@ describe(appConfig.name, () => {
   it(config.sitename, () => {
 
     /* THIS PART IS THE ACTUAL SCRAPER */
+
     cy.visit(config.URL);
 
+    /* LOGIN METHOD */
+
+    if(config.login.isLoginApplicable){
+      cy.get(config.login.usernameSelector).type(config.login.username);
+      cy.get(config.login.passwordSelector).type(config.login.password);
+      cy.get(config.login.submitSelector).click();
+      cy.wait(config.universalWaitTime);
+
+      /* SWEEPING THROUGH ACTION LINKS TO ARRIVE ON THE STARTING PAGE */
+      if(config.login.actionLinks.length > 0){
+        config.login.actionLinks.forEach((link)=>{
+          cy.get(link).click();
+          cy.wait(config.universalWaitTime);
+        });
+      }
+    }
+
     /* USER DEFINED PAGE SNAPPER */
+
     if (config.usePages) {
       config.pages.forEach((page, $i)=>{
         if (page===appConfig.tokens[0]) snapScreen(`/${config.sitename}/${appConfig.identifiers[0]}`);
           else{
             cy.get(page).click();
             cy.wait(config.universalWaitTime);
-            cy.screenshot(`/${config.sitename}/subpages/${$i}`);
+            snapScreen(`/${config.sitename}/subpages/${$i}`);
           }
       });
     }
